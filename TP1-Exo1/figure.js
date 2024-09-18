7728ff9e50562457ab0a424ef8550553db8920ed
Fredeau123 <frederictrivett@gmail.com>
2024-09-18 08:14:41 +0200

    update

--- TP1-Exo1/figure.js
+++ TP1-Exo1/figure.js
@@ -23,13 +23,38 @@ export default class Figure extends THREE.Group {
         });
     }
 
+    loadAnimation(model, animation) {
+        this.mixer = new THREE.AnimationMixer(model);
 
+        this.states = ["Idle", "Running", "Jump", "ThumbsUp"];
 
-    update() {
+        this.actions = {};
+
+        for (let i = 0; i < this.states.length; i++) {
+            const clip = animations[i];
+            if (this.state.includes(clip.name)) {
+                const action = this.mixer.clipAction(clip);
+                this.actions[clip.name] = action;
+                if (clip.name === "Jump" || clip.name === "ThumbsUp") {
+                    action.clampWhenFinished = true;
+                    action.loop = THREE.LoopOnce;
+                }
+            }
+        }
+
+        this.state = "Idle";
+        this.actions[this.state].play();
+    }
+
+    update(dt) {
+        // Vertical position
         this.position.y = this.params.y;
+        // Ground position
         this.position.x = this.params.x;
         this.position.z = this.params.z;
 
         this.rotation.y = this.params.ry;
+
+        this.mixer.update(dt);
     }
 }
